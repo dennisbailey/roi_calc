3b7b290bdf7f8c7488c50f64d9c492f3020fb61b
Dennis Bailey <bailey.dennis@gmail.com>
2016-06-28 15:05:28 -0600

    updated the data to break out revenues and expenses

--- src/js/data.js
+++ src/js/data.js
@@ -1,27 +1,28 @@
-var data = { revenues : [ { 
-                            description : 'Item 1',
-                            oneTime : 100, 
-                            monthly : 50
-                          },
-                          { 
-                            description : 'Item 2',
-                            oneTime : 50, 
-                            monthly : 25
-                          },
-                          { 
-                            description : 'Item 3',
-                            oneTime : 25, 
-                            monthly : 85
-                          },
-                        ],
-            expenses : [ { 
-                            description : 'Item 1',
-                            oneTime : 500, 
-                            monthly : 20
-                          },
-                          { 
-                            description : 'Item 2',
-                            oneTime : 200, 
-                            monthly : 40
-                          }]
-}
+var revenuesData = [ { 
+                   description : 'Item 1',
+                   oneTime : 100, 
+                   monthly : 50
+                 },
+                 { 
+                   description : 'Item 2',
+                   oneTime : 50, 
+                   monthly : 25
+                 },
+                 { 
+                   description : 'Item 3',
+                   oneTime : 25, 
+                   monthly : 85
+                 },
+               ];
+               
+var expensesData = [ { 
+                   description : 'Item 1',
+                   oneTime : 500, 
+                   monthly : 20
+                 },
+                 { 
+                   description : 'Item 2',
+                   oneTime : 200, 
+                   monthly : 40
+                 }
+               ];
