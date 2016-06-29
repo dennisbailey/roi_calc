6c8a7f1c50cf0745e19db723b520f63a6820819a
Dennis Bailey <bailey.dennis@gmail.com>
2016-06-29 10:25:02 -0600

    Formatting

--- src/js/components/rev-exp/rev-exp.controller.js
+++ src/js/components/rev-exp/rev-exp.controller.js
@@ -12,29 +12,28 @@ function RevExpCtrl($rootScope, revExpService) {
   vm.revenueData = revenueData;
   vm.expenseData = expenseData;
   
-  
   vm.deleteRevenue = function(index) {
     vm.revenueData = revExpService.deleteRevenue(index);
+    updateRevenueTotals();
+    updateCalcs();
   };
   
   vm.deleteExpense = function(index) {
-    vm.expenseData = revExpService.deleteExpense(index);;
+    vm.expenseData = revExpService.deleteExpense(index);
+    updateExpenseTotals();
+    updateCalcs();
   };
   
   var updateRevenueTotals = function() {
-    
     vm.revenuesOneTimeTotal = revenueData.reduce(function (total, obj) { return total + obj.oneTime; }, 0);
     vm.revenuesMonthlyTotal = revenueData.reduce(function (total, obj) { return total + obj.monthly; }, 0);
     vm.revenuesTotal = vm.revenuesOneTimeTotal + vm.revenuesMonthlyTotal * 12;
-
   };
   
   var updateExpenseTotals = function() {
-    
     vm.expensesOneTimeTotal = expenseData.reduce(function (total, obj) { return total + obj.oneTime; }, 0);
     vm.expensesMonthlyTotal = expenseData.reduce(function (total, obj) { return total + obj.monthly; }, 0);
     vm.expensesTotal = vm.expensesOneTimeTotal + vm.expensesMonthlyTotal * 12;
-    
   };
   
   var updateCalcs = function() {
@@ -42,7 +41,6 @@ function RevExpCtrl($rootScope, revExpService) {
     vm.contributionProfitTotal = vm.revenuesTotal - vm.expensesTotal;
     vm.contributionMargin = 100 * vm.contributionProfitTotal / vm.revenuesTotal;
     vm.capitalRoi = (vm.expensesOneTimeTotal - vm.revenuesOneTimeTotal) / vm.contributionProfitMonthly;
-    
   };
   
   // On page load, populate the inital values
