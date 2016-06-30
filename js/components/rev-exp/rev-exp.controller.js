4eca2acd26d5364a930a6094a8b015c7362d2f22
Dennis Bailey <bailey.dennis@gmail.com>
2016-06-30 14:21:07 -0600

    WIP edit functionality

--- js/components/rev-exp/rev-exp.controller.js
+++ js/components/rev-exp/rev-exp.controller.js
@@ -8,10 +8,29 @@ function RevExpCtrl($rootScope, revExpService) {
 
   var vm = this;
   
+  // Default show/hide values
+  vm.showAddRevenueBtn = true;
+  vm.showAddRevenueForm = false;
+  vm.showUpdateRevenueBtn = false;
+  vm.showAddExpenseBtn = true;
+  vm.showAddExpenseForm = false;
+  
+  // Functions to toggle between showing and hiding
+  vm.toggleHideAddRevenue = function() {
+    vm.showAddRevenueBtn = !vm.showAddRevenueBtn;
+    vm.showAddRevenueForm = !vm.showAddRevenueForm;
+  };
+  
+  vm.toggleHideAddExpense = function() {
+    vm.showAddExpenseBtn = !vm.showAddExpenseBtn;
+    vm.showAddExpenseForm = !vm.showAddExpenseForm;
+  };
+  
   // Attach the revenue and expense data from data.js to the scope
   vm.revenueData = revenueData;
   vm.expenseData = expenseData;
   
+  // Functions to delete revenue and expense line items
   vm.deleteRevenue = function(index) {
     vm.revenueData = revExpService.deleteRevenue(index);
     updateRevenueTotals();
@@ -24,28 +43,87 @@ function RevExpCtrl($rootScope, revExpService) {
     updateCalcs();
   };
   
-  vm.cancelRevenueAdd = function() {
-    vm.addRevenue = {};
+  // Functions to edit an existing revenue or expense
+  var preUpdateRevenue;
+  var preUpdateExpense;
+  
+  vm.editRevenue = function(index) {
+    vm.revenueIndex = index;
+    vm.showUpdateRevenueBtn = true;
+    vm.revenueFormTitle = 'Edit This Revenue Item';
+    vm.formRevenue = revenueData[index];
+    preUpdateRevenue = revenueData[index];
+    console.log('edit fn ', preUpdateRevenue);
+    vm.toggleHideAddRevenue();
   };
   
-  vm.cancelExpenseAdd = function() {
-    vm.addExpense = {};
-  }
+  vm.editExpense = function(index) {
+    vm.expenseIndex = index;
+    vm.showUpdateExpenseBtn = true;
+    vm.expenseFormTitle = 'Edit This Expense Item';
+    vm.formExpense = expenseData[index];
+    preUpdateExpense = expenseData[index];
+    vm.toggleHideAddExpense();
+  };
   
+  vm.updateRevenueItem = function() {
+    revenueData[vm.revenueIndex] = vm.formRevenue;
+    vm.formRevenue = {};
+    vm.showUpdateRevenueBtn = false;
+    vm.toggleHideAddRevenue();
+    updateRevenueTotals();
+    updateCalcs();
+  };
+  
+  vm.updateExpenseItem = function() {
+    rexpenseData[vm.expenseIndex] = vm.formExpense;
+    vm.formExpense = {};
+    vm.showUpdateExpenseBtn = false;
+    vm.toggleHideAddExpense();
+    updateExpenseTotals();
+    updateCalcs();
+  };
+  
+  // Functions to add a new revenue or expense to the data
   vm.submitRevenueAdd = function() {
-    revenueData.push(vm.addRevenue);
-    vm.addRevenue = {};
+    revenueData.push(vm.formRevenue);
+    vm.formRevenue = {};
     updateRevenueTotals();
     updateCalcs();
   };
   
   vm.submitExpenseAdd = function() {
-    expenseData.push(vm.addExpense);
-    vm.addExpense = {};
+    expenseData.push(vm.formExpense);
+    vm.formExpense = {};
     updateExpenseTotals();
     updateCalcs();
-  }
+  };
   
+  
+  // Functions to cancel adding or updating a revenue or expense
+  vm.cancelRevenueAdd = function() {
+    vm.formRevenue = {};
+  };
+  
+  vm.cancelExpenseAdd = function() {
+    vm.formExpense = {};
+  };
+  
+  vm.cancelRevenueUpdate = function() {
+    console.log(preUpdateRevenue);
+    revenueData[vm.revenueIndex] = preUpdateRevenue;
+    vm.revenueData = revenueData;
+    vm.formRevenue = {};
+    vm.showUpdateRevenueBtn = false;
+    vm.toggleHideAddRevenue();
+  };
+  
+  vm.cancelExpenseUpdate = function() {
+    expenseData[vm.expenseIndex] = vm.preUpdateExpense;
+    vm.formExpense = {};
+  };
+  
+  // Functions to update the calculations on the page
   var updateRevenueTotals = function() {
     vm.revenuesOneTimeTotal = revenueData.reduce(function (total, obj) { return total + obj.oneTime; }, 0);
     vm.revenuesMonthlyTotal = revenueData.reduce(function (total, obj) { return total + obj.monthly; }, 0);
