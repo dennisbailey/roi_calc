b6733832fe8632e0d1188c90b9291274e918276e
Dennis Bailey <bailey.dennis@gmail.com>
2016-06-30 20:46:35 -0600

    HTML5 mode

--- js/app.js
+++ js/app.js
@@ -3,5 +3,14 @@
   'use strict';
 
   angular.module('roiCalcApp', ['ngRoute']);
+  
+  angular.module('roiCalcApp', [])
+    
+    .config(function($locationProvider) {
+    
+        // use the HTML5 History API
+        $locationProvider.html5Mode(true);
+    });
 
 })();
+
